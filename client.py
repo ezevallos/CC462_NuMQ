b5d40dc45b1e8feb86fb71770718f40b5bb733e3
ezevallos <ezevalloslabarthe@gmail.com>
2020-07-19 15:43:32 -0500

    client

---
+++ client.py
@@ -0,0 +1,48 @@
+# !/usr/bin/env python
+
+import socket
+import select
+import errno
+import sys
+
+HEADER_LENGTH = 10
+
+# IP = "127.0.0.1" # CAMBIAR POR IP DE AWS
+IP = "3.15.232.180"
+# PORT = 1234 # CAMBIAR POR PUERTO DE AWS
+PORT = 5555
+
+my_username = input("Username: ")
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, PORT))
+client_socket.setblocking(False)
+
+username = my_username.encode('utf-8')
+username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
+client_socket.send(username_header + username)
+
+while True:
+	message = input(f"{my_username} > ")
+	try:
+		while True:
+			username_header = client_socket.recv(HEADER_LENGTH)
+			if not len(username):
+				print("connection closed by the server")
+				sys.exit()
+			username_length = int(username_length.decode('utf-8').strip())
+			username = client_socket.recv(username_length).decode('utf-8')
+
+			message_header = client_socket.recv(HEADER_LENGTH)
+			message_length = int(message_header.decode('utf-8').strip())
+			message = client_socket.recv(message_length).decode('utf-8')
+
+			print(f"{username} > {message}")
+	except IOError as e:
+		if e.errno != eerno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+			print('Reading error: ', str(e))
+			sys.exit()
+		continue
+	
+	except Exception as e:
+		print('General error: ', str(e))
+		sys.exit()
