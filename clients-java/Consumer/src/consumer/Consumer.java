e97113a1e07e90b878a85e0be1d6f8e150d4b04e
victor-alegre <victoralegre@uni.pe>
2020-07-21 01:13:15 -0500

    consumer ejemplo 1 java

---
+++ clients-java/Consumer/src/consumer/Consumer.java
@@ -0,0 +1,32 @@
+package consumer;
+
+import java.io.IOException;
+import numq.libs.Channel;
+import numq.libs.Connection;
+
+/**
+ *
+ * @author Victor
+ */
+public class Consumer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String hostAddr = "localhost";
+        int numPort = 5555;
+        String queueName = "hello";
+        try {
+            Connection connection = new Connection(hostAddr, numPort);
+            Channel channel = connection.getChannel();
+            channel.declareQueue(queueName);
+            channel.consume(queueName, true, (String body) -> {
+                System.out.println(body);
+            });
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+    
+}
